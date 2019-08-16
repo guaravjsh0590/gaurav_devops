65649b23bcdef424234ed99050f692cdca631a49
gaurav <gauravjsh0590@gmail.com>
2019-08-16 20:43:25 +0530

    This is my Java Project

---
+++ devopsProject/src/devopsProject/firstCode.java
@@ -0,0 +1,19 @@
+package devopsProject;
+
+public class firstCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Word");
+		System.out.println("This is my first JAVA Program for No Swap");
+		int a=10;
+		int b=5;
+		System.out.println("The value of a="+ a +" value of b="+ b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("The value after swapping for a="+a+" and for b="+b); 
+	}
+
+}
